7970a91acc4fdc372226c4b08508eb3bd78e2548
Ratnesh Dubey <dubeyratnesh60@gmail.com>
2023-09-21 20:18:20 +0530

    first commit

---
+++ src/main/java/com/example/rd/cool/Engine.java
@@ -0,0 +1,24 @@
+package com.example.rd.cool;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Engine {
+
+	private String engine = "V8";
+	private int cc = 2000;
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public int getCc() {
+		return cc;
+	}
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+	
+	
+}
---
+++ src/test/java/com/example/rd/cool/PehlaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rd.cool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PehlaSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
